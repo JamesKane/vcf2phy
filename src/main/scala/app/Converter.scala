e7ff5ab57233cee57a635cdfcff1f3ea862c8f32
jkane <jkane@tds.net>
2018-06-22 18:26:42 -0500

    -- Fixed the number of samples output when there are no valid called alleles.

--- src/main/scala/app/Converter.scala
+++ src/main/scala/app/Converter.scala
@@ -7,12 +7,12 @@ import java.util.logging.Logger
 
 object Converter extends App {
   val logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME)
-  val bases = Set('A', 'C', 'G', 'T')
+  val bases = Set("A", "C", "G", "T")
 
   // TODO:  Replace this with a more robust argument system
-  if(args.length < 3 || args(1) == "-h") {
+  if (args.length < 3 || args(1) == "-h") {
     println(s"Usage: ${args(0)} vcf_file_path phy_output_path")
-  }  else {
+  } else {
     val inputPathName = args(1)
     val outputPathName = args(2)
 
@@ -25,21 +25,19 @@ object Converter extends App {
         new OutputStreamWriter(
           new FileOutputStream(outputPathName), Charset.forName("UTF-8").newEncoder()))
 
-      fw.write(s"${matrix.sampleNames.length} ${matrix.rows.length}" + System.lineSeparator())
-      matrix.sampleNames.foreach(k => {
+      val names = matrix.sampleNames.filter(taxa => matrix.rows.map(_.status(taxa)).exists(bases.contains))
+      if (names.size != matrix.sampleNames.size) {
+        logger.warning("Sample(s) removed from cohort: " + (matrix.sampleNames.toSet - names).mkString(", "))
+      }
+      fw.write(s"${names.length} ${matrix.rows.length}" + System.lineSeparator())
+      names.foreach(k => {
         // Phylip format requires a 10 character row label for the organism
         val label = k.padTo(10, " ").slice(0, 10).mkString("")
 
         // Collect all the calls for the organism to a single string
         // TODO: Consider interleaving with a defined column count and group spacing
         val calls = matrix.rows.map(r => r.status(k)).mkString("")
-
-        if(calls.forall(!bases.contains(_))) {
-          logger.warning(s"$k has no A, C, G, or T bases!")
-        } else {
-          fw.write(s"$label$calls" + System.lineSeparator())
-        }
-
+        fw.write(s"$label$calls" + System.lineSeparator())
       })
     } finally {
       fw.close()
